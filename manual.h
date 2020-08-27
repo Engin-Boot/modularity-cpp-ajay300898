69a9790e049df8a4840d463fc6261b57672d1312
Ajay Kumar <41167768+ajay300898@users.noreply.github.com>
2020-08-27 13:20:11 +0530

    Update manual.h

--- manual.h
+++ manual.h
@@ -2,11 +2,23 @@
 #define MANUAL
 #include "telCoColorCoder.h"
 
-void getManualFromCode(int pairNumber)
+std::string getManualFromCode(int pairNumber)
 {
     TelCoColorCoder::ColorPair colorPair =
         TelCoColorCoder::GetColorFromPairNumber(pairNumber);
     std::cout << "Pair details : " << colorPair.ToString() << std::endl;
+    return colorPair.ToString();
 }
 
+std::string getManual(){
+    std::string manual = "-------> MANUAL <--------\n";
+    for(int major = WHITE; major <= VIOLET; major++){
+        for(int minor = BLUE; minor <= SLATE; minor++){
+            int pair = (major * numberOfMinorColors) + minor + 1;
+            manual += getManualFromCode(pair);
+            manual += "\n";
+        }
+    }
+    return manual;
+}
 #endif
